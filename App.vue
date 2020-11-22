f89fb39ea68cf0ea04494f74f906125616cc379a
Edwin Garcia <spark.com.co@gmail.com>
2020-11-22 16:14:24 -0500

    Utilizando JSX para la creacion de un componente

--- App.vue
+++ App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <button @click.prevent="toggleHandler">Toggle Modal</button>
-    <modal-create-element :show="show" message="Hello world!"></modal-create-element>
+    <!-- <modal-create-element :show="show" message="Hello world!"></modal-create-element> -->
+    <modal-jsx :show="show" message="Hello world!"></modal-jsx>
   </div>
 </template>
 
 <script>
-import ModalCreateElement from './ModalCreateElement.vue'
+// import ModalCreateElement from './ModalCreateElement.vue'
+import ModalJsx from './ModalJsx.vue'
 
 export default {
   name: "App",
   components: {
-    ModalCreateElement
+    // ModalCreateElement,
+    ModalJsx
   },
   data() {
     return {
---
+++ ModalJsx.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'ModalJsx',
+
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  },
+
+  render() {
+    return this.show ? <div class="modal">
+      <p>{this.message}</p>
+      <button onClick={this.clickCancelHandler}>Cancel</button>
+      <button onClick={this.clickOkHandler}>Ok</button>
+    </div> : null
+  },
+
+  methods: {
+    clickCancelHandler() {
+      console.log('Cancel click')
+    },
+    clickOkHandler() {
+      console.log('Ok click')
+    }
+  }
+}
+</script>
