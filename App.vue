fa9a27d3859c9564434a0a51812ea32a871f45ed
Edwin Garcia <spark.com.co@gmail.com>
2020-11-22 16:22:28 -0500

    Utilizando slots con render functions

--- App.vue
+++ App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     <button @click.prevent="toggleHandler">Toggle Modal</button>
-    <!-- <modal-create-element :show="show" message="Hello world!"></modal-create-element> -->
-    <modal-jsx :show="show" message="Hello world!"></modal-jsx>
+    <modal-create-element :show="show">
+      <h1>Modal title</h1>
+      <p>Hello world!</p>
+    </modal-create-element>
+    <!-- <modal-jsx :show="show" message="Hello world!"></modal-jsx> -->
   </div>
 </template>
 
 <script>
-// import ModalCreateElement from './ModalCreateElement.vue'
-import ModalJsx from './ModalJsx.vue'
+import ModalCreateElement from './ModalCreateElement.vue'
+// import ModalJsx from './ModalJsx.vue'
 
 export default {
   name: "App",
   components: {
-    // ModalCreateElement,
-    ModalJsx
+    ModalCreateElement,
+    // ModalJsx
   },
   data() {
     return {
