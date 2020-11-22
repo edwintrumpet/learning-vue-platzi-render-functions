46856b32eb02d6c3bdadfb1f39110791b717874a
Edwin Garcia <spark.com.co@gmail.com>
2020-11-22 15:52:07 -0500

    Creacion de un componente con createElement

--- App.vue
+++ App.vue
@@ -1,11 +1,27 @@
 <template>
   <div id="app">
-    <button>Toggle Modal</button>
+    <button @click.prevent="toggleHandler">Toggle Modal</button>
+    <modal-create-element :show="show" message="Hello world!"></modal-create-element>
   </div>
 </template>
 
 <script>
+import ModalCreateElement from './ModalCreateElement.vue'
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    ModalCreateElement
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    toggleHandler() {
+      this.show = !this.show
+    }
+  }
 }
 </script>
